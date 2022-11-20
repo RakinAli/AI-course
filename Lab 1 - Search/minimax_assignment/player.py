c6f24ba2935e256293d3da3eb95bde0c5e08a91c
Rakin Ali <rakinali00@gmail.com>
2022-11-20 15:41:58 +0100

    Added better heuristics
    Co-Authored-By: Filip Wetterdal Todorovic <81308705+Quroz@users.noreply.github.com>

--- Lab 1 - Search/minimax_assignment/player.py
+++ Lab 1 - Search/minimax_assignment/player.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import random
+import math
 
 from fishing_game_core.game_tree import Node
 from fishing_game_core.player_utils import PlayerController
@@ -66,28 +67,118 @@ class PlayerControllerMinimax(PlayerController):
         #       with its compute_and_get_children() method!
 
         node = initial_tree_node
-
         best_move = self.find_best_move(node)
-        
 
         return ACTION_TO_STR[best_move]
 
-    # 
+    #
     def find_best_move(self, node):
         """
         @param node: initial game tree node
         @return: best move
         """
         children = node.compute_and_get_children()
-
+        best_move = 0
+        highest_score = -math.inf
         for child in children:
+            score = self.alphabeta(child, -math.inf, math.inf, 0)
+            if (score > highest_score):
+                highest_score = score
+                best_move = child.move
+        return best_move
 
-    
-    def alphabeta(self,node,alpha,beta):
-        #Terminal node
-        if node.depth == 0 or node.state. :
-            return node.value
-        
+    def alphabeta(self, node, alpha, beta, depth):
+        score = 0
+        # Terminal node
+        if depth == 0 or len(node.children) == 0:
+            score = self.heuristics(node)
+            return score
 
+        state = node.state
+        # Maximizing player
+        if state.player == 0:
+            score = -math.inf
+            for child in node.children:
+                score = max(score, self.alphabeta(child, alpha, beta, depth-1))
+                alpha = max(alpha, score)
+                if beta <= alpha:
+                    break
+        # Minimizing player
+        else:
+            score = math.inf
+            for child in node.children:
+                score = min(score, self.alphabeta(child, alpha, beta, depth = depth-1))
+                beta = min(beta, score)
+                if beta <= alpha:
+                    break
+        return score
 
-        
+    # heuristics that evaluates the score of a state
+    def heuristics(self, node):
+        """
+        @param node: game tree node
+        @return: score of the state
+        """
+        diff = node.state.player_scores[0] - node.state.player_scores[1]
+
+        # Get the best fishes
+        best_fishes = self.best_fishes(node)
+
+        return (diff + best_fishes)
+
+    def best_fishes(self, node):
+        # Different
+        max_score = -math.inf
+        min_score = -math.inf
+        max_temp = 0
+        min_temp = 0
+
+        # Get the fish score of every fish
+        for fish in node.state.fish_positions.keys():
+            # Get the fish sore and position
+            fish_score = node.state.fish_scores[fish]
+            fish_pos = node.state.fish_positions[fish]
+
+            # Get the hook positions for MAX and MIN
+            hook_max = node.state.hook_positions[0]
+            hook_min = node.state.hook_positions[1]
+
+            # Get the distances between the fish and the hooks
+            distance_max = self.manhattan_distance(fish_pos, hook_max)
+            distance_min = self.manhattan_distance(fish_pos, hook_min)
+
+            # Get the value per distance for the fishes for MAX player
+            if distance_max == 0:
+                max_temp = fish_score * 2
+            else:
+                max_temp = fish_score / distance_max
+
+            # Get the value per distance for the fishes for MIN player
+            if distance_min == 0:
+                min_temp = fish_score
+            else:
+                min_temp = fish_score / distance_min
+
+            # Pick the best value for MAX and MIN player
+            if (max_temp > max_score):
+                max_score = max_temp
+            if (min_temp > min_score):
+                min_score = min_temp
+
+        # Player with the best score wins
+        # SE HÄR
+        return (max_score-min_score)
+
+    # Get the distance between the fish and the players
+
+    def manhattan_distance(self, pos1, pos2):
+        """
+        @param pos1: position of the fish
+        @param pos2: position of the player
+        @return: distance between the fish and the player"""
+
+        y = abs(pos1[1] - pos2[1])
+        x1 = abs(pos1[0] - pos2[0])
+        x = min(x1, 20 - x1)
+
+        return x + y
