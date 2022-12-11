f122d4293675ccdf269a938474d62ef01495cf6a
Rakin Ali <rakinali00@gmail.com>
2022-12-11 16:08:10 +0100

    Finish HMM3
    Co-Authored-By: Filip Wetterdal Todorovic <81308705+Quroz@users.noreply.github.com>

--- Lab 2 - HMM/HMM_3/HMM3.py
+++ Lab 2 - HMM/HMM_3/HMM3.py
@@ -63,7 +63,7 @@ def alpha_pass(transitions_matrix, emission_matrix, pi_matrix, emission_elements
     scale[0] += alpha[state][0] 
   
   # Scale alpha matrix for first observation like Mark Stamp (A Reavaling introduction to Hidden Markov Models)
-  scale[0]  = 1 / scale[0]
+  scale[0]  = (1/scale[0])
   for state in range(total_states):
     alpha[state][0] = scale[0] * alpha[state][0]
   
@@ -71,10 +71,9 @@ def alpha_pass(transitions_matrix, emission_matrix, pi_matrix, emission_elements
   for observation in range(1, total_observations):
     for from_state in range(total_states):
       # Calculate scaling factor --> sum of all alpha values for each observation
-      alpha[from_state][observation] = 0
       for to_state in range(total_states):
         # Calculate the probability of being in state i at time t given the observations up to current observation
-        alpha[from_state][observation] += alpha[from_state][observation -1]  * transitions_matrix[to_state][from_state]
+        alpha[from_state][observation] += alpha[to_state][observation -1]  * transitions_matrix[to_state][from_state]
       # Multiply with the probability of observing o_t given that we are in state i
       alpha[from_state][observation] *= emission_matrix[from_state][emission_elements[observation]]
       scale[observation] += alpha[from_state][observation]
@@ -83,32 +82,129 @@ def alpha_pass(transitions_matrix, emission_matrix, pi_matrix, emission_elements
     scale[observation] = 1 / scale[observation]
     for state in range(total_states):
       alpha[state][observation] = scale[observation] * alpha[state][observation] 
-  return alpha, scale
-       
 
+  return alpha, scale
 
 # @TODO --> Beta pass also known as backward pass algorithm
 # OBS, heavily inspired by Mark Stamp (A Reavaling introduction to Hidden Markov Models)
-def beta_pass(transitions_matrix, emission_matrix, pi_matrix, emission_elements, scale):
+def beta_pass(transitions_matrix, emission_matrix, emission_elements, scale):
+    
+  total_observations = len(emission_elements)
+  total_states = len(transitions_matrix)
 
-  return 0
+  # Create beta matrix
+  # [ 1= [ALLA OBSERVATIONER], 2= [ALLA OBSERVATIONER] ] 1 och 2 är states
+  beta = [[0 for x in range(total_observations)] for y in range(total_states)]
+
+  # Initialize beta matrix with last observation
+  for state in range(total_states):
+    beta[state][total_observations-1] = scale[total_observations-1]
+  
+  # Calculate beta matrix for all observations
+  for observation in range(total_observations-2, -1, -1):
+    for from_state in range(total_states):
+      beta[from_state][observation] = 0
+      for to_state in range(total_states):
+        beta[from_state][observation] += transitions_matrix[from_state][to_state] * emission_matrix[to_state][emission_elements[observation+1]] * beta[to_state][observation+1]
+      # Scale beta matrix for current observation like Mark Stamp (A Reavaling introduction to Hidden Markov Models)
+      beta[from_state][observation] = scale[observation] * beta[from_state][observation]
+  return beta
+
 
 # Heavily inspired by Mark Stamp (A Reavaling introduction to Hidden Markov Models)
 def digamma(alpha, beta, transitions_matrix, emission_matrix, emission_elements):
+  """@docstring
+  The digamma function is used to calculate the probability of being in state i at time t and state j at time t+1 given the observations up to time t+1.
+  The digamma function is a dynamic programming algorithm that uses the following recursion:
+  digamma_t(i,j) = alpha_t(i) * a_ij * b_j(o_t+1) * beta_t+1(j)
+  where:
+  alpha_t(i) = the probability of being in state i at time t given the observations up to time t
+  a_ij = the probability of transitioning from state i to state j
+  b_j(o_t+1) = the probability of observing o_t+1 given that we are in state j
+  beta_t+1(j) = the probability of being in state j at time t+1 given the observations from time t+1 to the end of the sequence
+  """
+  total_observation = len(emission_elements)
+  total_states = len(transitions_matrix)
 
-  return 0
+  # Create digamma and gamma matrix
+  gamma = [[0 for x in range(total_observation)] for y in range(total_states)]
+  di_gamma = [[[ 0 for x in range(total_observation)] for y in range(total_states)] for z in range(total_states)]
 
-
+  # Calculate digamma and gamma matrix
+  for observation in range(total_observation-1):
+    for from_state in range(total_states):
+      for to_state in range(total_states):
+        # Calculate the probability of being in state i at time t and state j at time t+1 given the observations up to time t+1
+        di_gamma[to_state][from_state][observation] = alpha[from_state][observation] * transitions_matrix[from_state][to_state] * emission_matrix[to_state][emission_elements[observation+1]] * beta[to_state][observation+1]
+        # Calculate the probability of being in state i at time t given the observations up to time t
+        gamma[from_state][observation] += di_gamma[to_state][from_state][observation]
+  
+  for state in range(total_states):
+    gamma[state][total_observation-1] = alpha[state][total_observation-1]
+  return gamma, di_gamma
+  
 # Heavily inspired by Mark Stamp (A Reavaling introduction to Hidden Markov Models)
 def re_estimate_model_parameters(gamma, di_gamma, transitions_matrix, emission_matrix, pi_matrix, emission_elements):
+  """@docstring
+  The re-estimation step is used to update the model parameters. The re-estimation step is a dynamic programming algorithm that uses the following recursion:
+  a_ij = sum_t(digamma_t(i,j)) / sum_t(gamma_t(i))
+  b_j(o_t) = sum_t(digamma_t(i,j)) / sum_t(gamma_t(i))
+  pi_i = gamma_1(i)
+  where:
+  digamma_t(i,j) = the probability of being in state i at time t and state j at time t+1 given the observations up to time t+1
+  gamma_t(i) = the probability of being in state i at time t given the observations up to time t
+  """
+  total_observation = len(emission_elements)
+  total_states = len(transitions_matrix)
 
-  return 0
+  # Re-estimate the initial state probabilities
+  for state in range(total_states):
+    pi_matrix[0][state] = gamma[state][0]
+  
+  # Re-estimate the transition probabilities
+  for from_state in range(total_states):
+    denominator = 0
+    for observation in range(total_observation-1):
+      # Calculate the denominator for the transition probabilities
+      denominator += gamma[from_state][observation]
+    for to_state in range(total_states):
+      numerator = 0
+      for observation in range(total_observation-1):
+        # Calculate the numerator for the transition probabilities
+        numerator += di_gamma[to_state][from_state][observation]
+      # Calculate the transition probabilities
+      transitions_matrix[from_state][to_state] = numerator / denominator
+  
+  # Re-estimate the emission probabilities
+  for from_state in range(total_states):
+    denominator = 0
+    for observation in range(total_observation):
+      # Calculate the denominator for the emission probabilities
+      denominator += gamma[from_state][observation]
+    for emission in range(len(emission_matrix[0])):
+      numerator = 0
+      for observation in range(total_observation):
+        # Check if the emission is the same as the observation
+        if emission_elements[observation] == emission:
+          numerator += gamma[from_state][observation]
+      # Calculate the emission probabilities
+      emission_matrix[from_state][emission] = numerator / denominator      
+
+  return transitions_matrix, emission_matrix, pi_matrix
+      
 
 # Heavily inspired by Mark Stamp (A Reavaling introduction to Hidden Markov Models)
-def calculate_log_likelihood(scale):
-  return 0
-
-
+def log_likelyhood(scale):
+  """@docstring
+  The log likelyhood is the sum of the log of the scaling factor
+  """
+  total_observation = len(scale)
+  log_prob = 0
+  for observation in range(total_observation):
+    log_prob += math.log(scale[observation])
+  log_prob = -log_prob
+  return log_prob
+  
 def learning_algorithm(transitions_matrix, emission_matrix, pi_matrix, emission_elements, max_iterations):
   """Docstring
   # 1) Do forward algorithm and get alpha and scaling factor
@@ -118,12 +214,33 @@ def learning_algorithm(transitions_matrix, emission_matrix, pi_matrix, emission_
   # do check convergence
   # do output if converged
   """
-
+  log_prob = -math.inf
   alpha, scaling_factor = alpha_pass(transitions_matrix, emission_matrix, pi_matrix, emission_elements)
 
+  for iteration in range(max_iterations):
+    # Calculate the alpha, beta and gamma matrix
+    alpha, scaling_factor = alpha_pass(transitions_matrix, emission_matrix, pi_matrix, emission_elements)
+    beta = beta_pass(transitions_matrix, emission_matrix, emission_elements, scaling_factor)
+    gamma, di_gamma = digamma(alpha, beta, transitions_matrix, emission_matrix, emission_elements)
+
+    # Re-estimate the model parameters
+    transition_new, emission_new, pi_new = re_estimate_model_parameters(gamma, di_gamma, transitions_matrix, emission_matrix, pi_matrix, emission_elements)
+
+    probability = log_likelyhood(scaling_factor)
+
+    if probability > log_prob:
+      log_prob = probability
+    else:
+      break
+
+  return transition_new, emission_new
+
+def answer(matrix):
+  print(str(len(matrix)) + ' ' + str(len(matrix[0])), end=' ')
+  for row in matrix:
+    for element in row:
+      print(round(element, 6), end=" ") 
   
-  
-  return 0
 
 
 def main():
@@ -135,12 +252,16 @@ def main():
   emission_matrix = create_matrix(B_guess)
   pi_matrix = create_matrix(pi_guess)
   emission_elements = emission_sequence[1:]
-  
 
   transition_output, emission_output  = learning_algorithm(transitions_matrix, emission_matrix, pi_matrix, emission_elements, 100)
-  #print_matrix(transition_output)
-  #print_matrix(emission_output)
+
+  # Print the output
+  answer(transition_output)
+  print()
+  answer(emission_output)
 
 
 if __name__ == '__main__':
   main()
+
+
